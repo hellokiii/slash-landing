5834ae64c7c67f8faf99c46b15b4722c64211571
yhan.bae <yhan.bae@navercorp.com>
2023-03-21 23:18:09 +0900

    디자인 파일 반영

--- index.js
+++ index.js
@@ -18,22 +18,14 @@ let observer = new IntersectionObserver(activateStep3, options);
 let section3 = document.querySelector('.section3');
 let section4 = document.querySelector('.section4');
 let section5 = document.querySelector('.section5');
-observer.observe(section3);
-observer.observe(section4);
-observer.observe(section5);
+// observer.observe(section3);
+// observer.observe(section4);
+// observer.observe(section5);
 
-if ('content' in document.createElement('template')) {
-  // Instantiate the table with the existing HTML tbody
-  // and the row with the template
-  const section1Bottom = document.querySelector('.section1-bottom');
-  const template = document.querySelector('#ios-banner');
-
-  // Clone the new row and insert it into the table
-  const clone = template.content.cloneNode(true);
-
-
-  section1Bottom.appendChild(clone);
-
-} else {
-  console.log('stop');
-}
+addEventListener('DOMContentLoaded', () => {
+  Array.from(document.querySelectorAll('section')).map((el) => {
+    const { clientHeight } = el;
+    const browserHeight = document.documentElement.clientHeight;
+    el.style.top = `${Math.min(0, browserHeight - clientHeight)}px`;
+  });
+});
