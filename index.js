45ae45df00a2fc29a8b445a354f0530a3ca1c88b
yhan.bae <yhan.bae@navercorp.com>
2023-03-26 17:25:07 +0900

    sideNav transition

--- index.js
+++ index.js
@@ -1,31 +1,83 @@
-const options = {
-  rootMargin: '100px',
-  threshold: 1.0,
-};
-function activateStep3(e) {
-  const observerEntry = e[0];
-  const targetClassName = observerEntry.target.className;
-  const sectionNumber = targetClassName.match(/section(\d)/)[1];
-  const stepClass = `step${sectionNumber - 2}`;
+let observers = [null, null, null];
 
-  if (observerEntry.isIntersecting) {
-    console.log(targetClassName, observerEntry.isIntersecting, observerEntry.intersectionRatio);
-    document.querySelector('.side-nav .active').classList.remove('active');
-    document.querySelector(`.side-nav .${stepClass}`).classList.add('active');
+function observeSideNavTransition() {
+  const sections = document.querySelectorAll('.intersection');
+  Array.from(sections).map((section) => {
+    const sectionNumber = section.className.match(/section(\d)/)[1];
+    observers[sectionNumber - 3]?.disconnect();
+    observers[sectionNumber - 3] = new IntersectionObserver(activateStep, {
+      threshold: 0.5,
+    });
+    observers[sectionNumber - 3].observe(section);
+  });
+
+  function activateStep(e) {
+    const { isIntersecting, intersectionRatio, target, boundingClientRect } = e[0];
+    const targetClassName = target.className;
+    const sectionNumber = +targetClassName.match(/section(\d)/)[1];
+    const activedStep = document.querySelector('.side-nav .active');
+    const activatedStepNumber = +activedStep.className.match(/step(\d)/)[1];
+    // 스크롤 내려갈때
+    if (isIntersecting) {
+      if (activatedStepNumber === sectionNumber - 3) {
+        document.querySelector('.side-nav .active').classList.remove('active');
+        document.querySelector(`.side-nav .step${sectionNumber - 2}`).classList.add('active');
+      }
+    }
+    // 다시 올라갈때
+    if (!isIntersecting && intersectionRatio !== 0 && boundingClientRect.top > 0) {
+      if (sectionNumber !== 3 && activatedStepNumber === sectionNumber - 2) {
+        document.querySelector('.side-nav .active').classList.remove('active');
+        document.querySelector(`.side-nav .step${sectionNumber - 3}`).classList.add('active');
+      }
+    }
   }
 }
-let observer = new IntersectionObserver(activateStep3, options);
-let section3 = document.querySelector('.section3');
-let section4 = document.querySelector('.section4');
-let section5 = document.querySelector('.section5');
-// observer.observe(section3);
-// observer.observe(section4);
-// observer.observe(section5);
 
-addEventListener('DOMContentLoaded', () => {
+function setSectionsStickyPosition() {
   Array.from(document.querySelectorAll('section')).map((el) => {
     const { clientHeight } = el;
     const browserHeight = document.documentElement.clientHeight;
     el.style.top = `${Math.min(0, browserHeight - clientHeight)}px`;
   });
+}
+
+function setSideNavStickyPosition() {
+  const { clientHeight: section3Height } = document.querySelector('.section3');
+  const { clientHeight: section4Height } = document.querySelector('.section4');
+  const { clientHeight: section5Height } = document.querySelector('.section5');
+  const navContainer = document.querySelector('.nav-section');
+
+  navContainer.style.paddingTop = `${section3Height / 2}px`;
+  navContainer.style.paddingBottom = `${section5Height / 2}px`;
+  navContainer.style.height = `${section3Height + section4Height + section5Height}px`;
+
+  const stickyNav = document.querySelector('.sticky');
+  const stickyNavTop = `${document.documentElement.clientHeight / 2 - stickyNav.clientHeight / 2}px`;
+  stickyNav.style.top = stickyNavTop;
+}
+
+// refresh 후 스크롤 리셋
+if (history.scrollRestoration) {
+  history.scrollRestoration = 'manual';
+} else {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+}
+
+addEventListener('load', () => {
+  setSectionsStickyPosition();
+  observeSideNavTransition();
+});
+
+addEventListener('resize', () => {
+  setSectionsStickyPosition();
+  setSideNavStickyPosition();
+});
+
+const section3Video = document.querySelector('video.section3-main');
+section3Video.addEventListener('loadeddata', () => {
+  setSectionsStickyPosition();
+  setSideNavStickyPosition();
 });
