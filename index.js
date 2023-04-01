273775cde76334702e79eacb1021e3afaec369fa
yhan.bae <yhan.bae@navercorp.com>
2023-04-01 22:39:34 +0900

    side sticky nav 수정

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-let observers = [null, null, null];
-
-function observeSideNavTransition() {
-  const sections = document.querySelectorAll('.intersection');
-  Array.from(sections).map((section) => {
-    const sectionNumber = section.className.match(/section(\d)/)[1];
-    observers[sectionNumber - 3]?.disconnect();
-    observers[sectionNumber - 3] = new IntersectionObserver(activateStep, {
-      threshold: 0.5,
-    });
-    observers[sectionNumber - 3].observe(section);
-  });
-
-  function activateStep(e) {
-    const { isIntersecting, intersectionRatio, target, boundingClientRect } = e[0];
-    const targetClassName = target.className;
-    const sectionNumber = +targetClassName.match(/section(\d)/)[1];
-    const activedStep = document.querySelector('.side-nav .active');
-    const activatedStepNumber = +activedStep.className.match(/step(\d)/)[1];
-    // 스크롤 내려갈때
-    if (isIntersecting) {
-      if (activatedStepNumber === sectionNumber - 3) {
-        document.querySelector('.side-nav .active').classList.remove('active');
-        document.querySelector(`.side-nav .step${sectionNumber - 2}`).classList.add('active');
-      }
-    }
-    // 다시 올라갈때
-    if (!isIntersecting && intersectionRatio !== 0 && boundingClientRect.top > 0) {
-      if (sectionNumber !== 3 && activatedStepNumber === sectionNumber - 2) {
-        document.querySelector('.side-nav .active').classList.remove('active');
-        document.querySelector(`.side-nav .step${sectionNumber - 3}`).classList.add('active');
-      }
-    }
-  }
-}
-
 function setSectionsStickyPosition() {
   Array.from(document.querySelectorAll('section.sticky')).map((el) => {
     const { clientHeight } = el;
@@ -52,7 +16,7 @@ function setSideNavStickyPosition() {
   navContainer.style.paddingBottom = `${section5Height / 2}px`;
   navContainer.style.height = `${section3Height + section4Height + section5Height}px`;
 
-  const stickyNav = document.querySelector('.sticky');
+  const stickyNav = document.querySelector('nav.sticky');
   const stickyNavTop = `${document.documentElement.clientHeight / 2 - stickyNav.clientHeight / 2}px`;
   stickyNav.style.top = stickyNavTop;
 }
@@ -68,7 +32,6 @@ if (history.scrollRestoration) {
 
 addEventListener('load', () => {
   setSectionsStickyPosition();
-  observeSideNavTransition();
   setSideSliderEvent();
   addEventListener('scroll', handleScroll);
 });
@@ -127,6 +90,11 @@ function handleScroll() {
     const previouseSectionHeight = previouseSection.clientHeight;
     const opacity = Math.min(1, (sectionCurrentTop - 10) / (previouseSectionHeight / 2));
     previouseSection.style.opacity = opacity;
+  }
+
+  if (sectionNumber >= 3 && sectionNumber <= 5) {
+    document.querySelector('.side-nav .active').classList.remove('active');
+    document.querySelector(`.side-nav .step${sectionNumber - 2}`).classList.add('active');
   }
 }
 
